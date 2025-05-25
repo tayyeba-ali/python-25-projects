54e8e24516a0c57e4ffdbca4b515e8160162b50e
Tayyeba Ali <155741204+tayyeba-ali@users.noreply.github.com>
2025-05-26 01:19:39 +0500

    Update image.py

--- 14 Photo manipulation in Python/image.py
+++ 14 Photo manipulation in Python/image.py
@@ -1,47 +1,41 @@
+
 import numpy as np
-import png
+from PIL import Image as PILImage
+import os
 
 class Image:
     def __init__(self, x_pixels=0, y_pixels=0, num_channels=0, filename=''):
-        # you need to input either filename OR x_pixels, y_pixels, and num_channels
         self.input_path = 'input/'
         self.output_path = 'output/'
-        if x_pixels and y_pixels and num_channels:
+
+        os.makedirs(self.input_path, exist_ok=True)
+        os.makedirs(self.output_path, exist_ok=True)
+
+        if filename:
+            self.array = self.read_image(filename)
+            self.y_pixels, self.x_pixels, self.num_channels = self.array.shape
+        elif x_pixels and y_pixels and num_channels:
             self.x_pixels = x_pixels
             self.y_pixels = y_pixels
             self.num_channels = num_channels
-            self.array = np.zeros((x_pixels, y_pixels, num_channels))
-        elif filename:
-            self.array = self.read_image(filename)
-            self.x_pixels, self.y_pixels, self.num_channels = self.array.shape
+            self.array = np.zeros((y_pixels, x_pixels, num_channels), dtype=np.float32)
         else:
-            raise ValueError("You need to input either a filename OR specify the dimensions of the image")
+            raise ValueError("You must provide either dimensions or a filename")
 
     def read_image(self, filename, gamma=2.2):
-        '''
-        read PNG RGB image, return 3D numpy array organized along Y, X, channel
-        values are float, gamma is decoded
-        '''
-        im = png.Reader(self.input_path + filename).asFloat()
-        resized_image = np.vstack(list(im[2]))
-        resized_image.resize(im[1], im[0], 3)
-        resized_image = resized_image ** gamma
-        return resized_image
+        image = PILImage.open(self.input_path + filename).convert('RGB')
+        image_np = np.asarray(image).astype(np.float32) / 255.0
+        image_np = np.power(image_np, gamma)
+        return image_np
 
     def write_image(self, output_file_name, gamma=2.2):
-        '''
-        3D numpy array (Y, X, channel) of values between 0 and 1 -> write to png
-        '''
         im = np.clip(self.array, 0, 1)
-        y, x = self.array.shape[0], self.array.shape[1]
-        im = im.reshape(y, x*3)
-        writer = png.Writer(x, y)
-        with open(self.output_path + output_file_name, 'wb') as f:
-            writer.write(f, 255*(im**(1/gamma)))
-
-        self.array.resize(y, x, 3)  # we mutated the method in the first step of the function
-        
+        im_gamma = np.power(im, 1 / gamma)
+        im_uint8 = (im_gamma * 255).astype(np.uint8)
+        output_image = PILImage.fromarray(im_uint8)
+        output_image.save(self.output_path + output_file_name)
+        print(f"Image saved: {self.output_path + output_file_name}")
 
 if __name__ == '__main__':
-    im = Image(filename='lake.png')
-    im.write_image('test.png')
+    im = Image(filename='lake.png')  # Put lake.png inside the input/ folder
+    im.write_image('test.png')       # It will save to output/test.png
